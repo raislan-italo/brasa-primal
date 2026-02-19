ce83a8e28be86c533950a14abd7303dd956de66b
Raislan Ítalo <raislanitalo62@gmail.com>
2026-02-18 23:45:13 -0300

    Corrigido bug do favicon mais uma vez

--- vite.config.ts
+++ vite.config.ts
@@ -21,13 +21,13 @@ export default defineConfig({
         description: "O Fogo Perfeito. Sem Espera.",
         icons: [
           {
-            src: "/icon-192.png",
+            src: "/icon-192.png?v=2",
             sizes: "192x192",
             type: "image/png",
             purpose: "any maskable"
           },
           {
-            src: "/icon-512.png",
+            src: "/icon-512.png?v=2",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable"
